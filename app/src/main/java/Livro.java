74bebed3aafeb010e3ec16690ac9cceb319847ce
ruangabreu@hotmail.com <ruangabreu@hotmail.com>
2018-08-13 08:34:16 -0300

    Projeto

---
+++ app/src/main/java/Livro.java
@@ -0,0 +1,14 @@
+public class Livro {
+    private String titulo;
+
+
+    public Livro( String tit){
+    this.titulo = tit;
+    }
+    public String getTitulo(){
+        return titulo;
+    }
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+}
---
+++ app/src/main/java/com/example/alunos/feedbook/Feedbook.java
@@ -0,0 +1,13 @@
+package com.example.alunos.feedbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Feedbook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedbook);
+    }
+}
